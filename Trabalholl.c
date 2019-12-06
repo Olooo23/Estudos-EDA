9767a491d902e793884ddae76900a45a4d5b58a6
Olooo23 <49620660+Olooo23@users.noreply.github.com>
2019-12-06 14:24:10 -0300

    Update Trabalholl.c

--- Trabalholl.c
+++ Trabalholl.c
@@ -2,16 +2,18 @@
 #include <stdio.h>
 #include <omp.h>
 #include <sys/time.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
-#define QTDE_LINHAS 200
-#define QTDE_COLUNAS 200
-#define TAM_VETOR 200
+#define QTDE_LINHAS 20
+#define QTDE_COLUNAS 20
+#define TAM_VETOR 20
 
 // Ax = b
 int Matriz[QTDE_LINHAS][QTDE_COLUNAS]; // nao precisa ser matriz quadrada
 int Vetor_x[TAM_VETOR];  // tamanho do vetor x deve ser igual ao numero de colunas da matriz A
 int Vetor_b[QTDE_LINHAS]; // vetor resultado (B) deve ter o mesmo numero de linha da matriz A
-
+int Matriz_Temporaria[QTDE_LINHAS][QTDE_COLUNAS];
 
 //Função para gerar a matriz e o vetor.
 int Gera_Matriz_Vetor () {
@@ -36,7 +38,6 @@ int Gera_Matriz_Vetor () {
 // Multiplicação sequencial do vetor pela matriz.
 // Implementa a função Ax=b e pega o tempo sequencial.
 int matvecSequencial(){
-	int Matriz_Temporaria[QTDE_LINHAS][QTDE_COLUNAS];
 	struct timeval tv1, tv2;
 	double t1, t2;
 
@@ -69,34 +70,30 @@ int matvecSequencial(){
 //Implementa a funcao Ax=b e coleta o tempo paralelo em OpenMP.
 
 int matvecHost() {
-	int Thread_ID;
-	int i, j;
-	int Matriz_Temporaria[QTDE_LINHAS][QTDE_COLUNAS];
+	int tid, i, j;
 	struct timeval tv1, tv2;
 	double t1, t2;
 
 	printf("\nExecutando MatvecHost: \n");
 
+	for (int i = 0 ; i < QTDE_LINHAS;i++) {
+		Vetor_b[i] = 0;
+	}
 	//Marcação do tempo Inicial.
 	gettimeofday(&tv1, NULL);
 	t1 = (double)(tv1.tv_sec) + (double)(tv1.tv_usec)/1000000.00;
 
-	//O numero de Thread é igual a quantidade de colunas da Matriz.
-	omp_set_num_threads(QTDE_COLUNAS);
-	
-	#pragma omp parallel private (Thread_ID, i, j) shared (Matriz, Vetor_b, Vetor_x, Matriz_Temporaria) 
-	{
-		Thread_ID = omp_get_thread_num();
-		for (i = 0; i < QTDE_LINHAS; i++) {
-			Matriz_Temporaria[i][Thread_ID] = Matriz[i][Thread_ID] * Vetor_x[Thread_ID];
-		}
+	#pragma omp parallel for shared(i) num_threads(QTDE_LINHAS) private(tid)
+  	for (i = 0; i < QTDE_LINHAS; i++)
+	 {
+	   for (int j = 0; j < QTDE_COLUNAS; j++)
+	   {
+	     //tid = omp_get_thread_num();
+	     //printf("\nTHREAD: %d LINHA: %d COLUNA: %d",tid,i,j);
+	     Vetor_b[i] += Vetor_x[j] * Matriz[i][j];
+	   }
+  }
 
-		//Gera o vetor de resultado B.
-		for (j = 0; j < QTDE_COLUNAS; j++) {
-			Vetor_b[Thread_ID]+= Matriz_Temporaria[Thread_ID][j];
-		}
-	}
-	//Obtem o tempo final.
 	gettimeofday(&tv2, NULL);
 	t2 = (double)(tv2.tv_sec) + (double)(tv2.tv_usec)/1000000.00;
 
